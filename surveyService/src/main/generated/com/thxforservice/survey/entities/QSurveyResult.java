a60d9dbd56325b32358a0f9187310a3e632525b5
yonggyo1125 <yonggyo00@kakao.com>
2024-09-11 10:54:53 +0900

    .

--- surveyService/src/main/generated/com/thxforservice/survey/entities/QSurveyResult.java
+++ surveyService/src/main/generated/com/thxforservice/survey/entities/QSurveyResult.java
@@ -7,6 +7,7 @@ import com.querydsl.core.types.dsl.*;
 import com.querydsl.core.types.PathMetadata;
 import javax.annotation.processing.Generated;
 import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
 
 
 /**
@@ -17,18 +18,43 @@ public class QSurveyResult extends EntityPathBase<SurveyResult> {
 
     private static final long serialVersionUID = 626042907L;
 
+    private static final PathInits INITS = PathInits.DIRECT2;
+
     public static final QSurveyResult surveyResult = new QSurveyResult("surveyResult");
 
+    public final StringPath answerData = createString("answerData");
+
+    public final StringPath email = createString("email");
+
+    public final NumberPath<Long> prgrsNo = createNumber("prgrsNo", Long.class);
+
+    public final NumberPath<Long> studentNo = createNumber("studentNo", Long.class);
+
+    public final QSurveyInfo surveyInfo;
+
+    public final NumberPath<Long> totScr = createNumber("totScr", Long.class);
+
+    public final StringPath username = createString("username");
+
     public QSurveyResult(String variable) {
-        super(SurveyResult.class, forVariable(variable));
+        this(SurveyResult.class, forVariable(variable), INITS);
     }
 
     public QSurveyResult(Path<? extends SurveyResult> path) {
-        super(path.getType(), path.getMetadata());
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
     }
 
     public QSurveyResult(PathMetadata metadata) {
-        super(SurveyResult.class, metadata);
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QSurveyResult(PathMetadata metadata, PathInits inits) {
+        this(SurveyResult.class, metadata, inits);
+    }
+
+    public QSurveyResult(Class<? extends SurveyResult> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.surveyInfo = inits.isInitialized("surveyInfo") ? new QSurveyInfo(forProperty("surveyInfo")) : null;
     }
 
 }
--- surveyService/src/main/java/com/thxforservice/survey/entities/SurveyResult.java
+++ surveyService/src/main/java/com/thxforservice/survey/entities/SurveyResult.java
@@ -1,12 +1,31 @@
 package com.thxforservice.survey.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name="SRVY_ANS_RSLT")
 public class SurveyResult {
+    @Id @GeneratedValue
+    @Column(name="srvyPrgrsNo")
+    private Long prgrsNo;
+
+    @JoinColumn(name="srvyNo")
+    @ManyToOne(fetch=FetchType.LAZY)
+    private SurveyInfo surveyInfo;
+
+    private Long studentNo;
+
+    @Column(length=80, nullable = false)
+    private String email; // 로그인 회원 이메일
+
+    @Column(length=40, nullable = false)
+    private String username; // 로그인 회원명
+
+    @Lob
+    private String answerData; // 학생이 답한 질문 데이터
+
+    private Long totScr; // 총 점수
 
 }
