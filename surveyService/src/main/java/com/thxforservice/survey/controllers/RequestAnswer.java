848827783a73f64c6dc84f646b134ca33c08dfa4
yonggyo1125 <yonggyo00@kakao.com>
2024-09-13 08:48:38 +0900

    .

--- surveyService/src/main/java/com/thxforservice/survey/controllers/RequestAnswer.java
+++ surveyService/src/main/java/com/thxforservice/survey/controllers/RequestAnswer.java
@@ -1,24 +1,13 @@
 package com.thxforservice.survey.controllers;
 
-import com.thxforservice.survey.entities.SurveyInfo;
-import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
 
 @Data
 public class RequestAnswer {
 
-
-    private Long prgrsNo;
-    private String mode = "write"; // write :  작성, update -  수정
-    private Long studentNo;
-
-    private String email; // 로그인 회원 이메일
-
-    private String username; // 로그인 회원명
-
-    private String answerData; // 학생이 답한 질문 데이터, 각 문항점수는 json으로 해서 총점계산
-
-    private Long totScr; // 총 점수
-
-    private Long srvyNo; // 추가: 설문 ID
+    private Long srvyNo;
+    private List<Map<String, String>> answers;
 }
